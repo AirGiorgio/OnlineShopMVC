e340a436c94ae1278ef630d495b1cf38a0b810fa
Jan Klata <Barty7199@onet.pl>
2023-07-12 12:21:15 +0200

    Remove ignored files

--- OnlineShopMvc.App/DTOs/RoleDTOs/RoleDTO.cs
+++ OnlineShopMvc.App/DTOs/RoleDTOs/RoleDTO.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using OnlineShopMvc.App.DTOs.ProductDTOs;
 using OnlineShopMvc.App.Mapping;
+using OnlineShopMvc.Domain.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +13,16 @@ namespace OnlineShopMvc.App.DTOs.RoleDTOs
 {
     public class RoleDTO : IMapFrom<IdentityRole>
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NormalizedName { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<IdentityRole, RoleDTO>().ReverseMap()
+                .ForMember(x => x.Id, opt => opt.MapFrom(s => s.Id))
+                .ForMember(x => x.Name opt => opt.MapFrom(s => s.Name))
+                .ForMember(x => x.NormalizedName, opt => opt.MapFrom(s => s.NormalizedName));
+        }
     }
 }
