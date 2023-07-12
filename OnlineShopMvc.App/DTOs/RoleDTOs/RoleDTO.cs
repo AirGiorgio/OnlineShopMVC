f6736850726bcf6af1eddfae805e9554fc0d7f84
Jan Klata <Barty7199@onet.pl>
2023-07-12 09:42:19 +0200

    fixed

---
+++ OnlineShopMvc.App/DTOs/RoleDTOs/RoleDTO.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using OnlineShopMvc.App.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopMvc.App.DTOs.RoleDTOs
+{
+    public class RoleDTO : IMapFrom<IdentityRole>
+    {
+    }
+}
---
+++ OnlineShopMvc.App/DTOs/UserDTOs/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using OnlineShopMvc.App.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopMvc.App.DTOs.UserDTOs
+{
+    public class UserDTO : IMapFrom<IdentityUser>
+    {
+    }
+}
